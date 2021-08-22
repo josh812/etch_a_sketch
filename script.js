4c5dba8ed144dc17d08553d819d2eaf06d1d2cac
Josh <joshua.mccrory@icloud.com>
2021-08-22 09:31:31 -0700

    Drawing board now exists

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function changeColor() {
+    this.classList.add('drawn');
+}
 const container = document.querySelector('#container');
+
+
+for(let i=1; i<=15; i++) {
+    for(let j=1; j<=16; j++) {
+    newdiv = document.createElement('div');
+    newdiv.classList.add('square');
+    container.appendChild(newdiv);
+    }
+}
+
+const squares = container.querySelectorAll('.square');
+squares.forEach((square) => {
+    square.addEventListener('mouseenter', changeColor);
+});
